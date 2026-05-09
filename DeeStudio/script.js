12f0b1cf1c6adb04ea5a050a2b3918176bba5856
deepakgammer <134363904+deepakgammer@users.noreply.github.com>
2026-05-09 10:22:56 +0530

    Update script.js

--- DeeStudio/script.js
+++ DeeStudio/script.js
@@ -1,6 +1,6 @@
-const menuBtn = document.getElementById('menuBtn');
-const navLinks = document.getElementById('navLinks');
+const menuBtn = document.getElementById("menuBtn");
+const navLinks = document.getElementById("navLinks");
 
-menuBtn.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
 });
